389ab5eaef6b91224e5ee868e010780b814e4ef0
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-08-31 20:08:03 +0200

    SumWithoutHighestAndLowestNumber

---
+++ src/SumWithoutHighestAndLowestNumber.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SumWithoutHighestAndLowestNumber {
+
+    public static int sum(int[] numbers) {
+
+        int sum = 0;
+        Arrays.sort(numbers);
+
+        for (int i = 1; i < numbers.length - 1; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+}
