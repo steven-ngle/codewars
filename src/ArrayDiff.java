9d3e065a6129138cdc293dc6b5c225d86fb46c4e
Steven Nguyen <stengu51202@gmail.com>
2023-09-03 15:06:54 +0200

    commit

---
+++ src/ArrayDiff.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class ArrayDiff {
+// tbd
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(arrayDiff(new int[] {1,2}, new int[] {1}))); // {2}
+        System.out.println(Arrays.toString(arrayDiff(new int[] {1,2,2}, new int[] {1}))); // {2,2}
+        System.out.println(Arrays.toString(arrayDiff(new int[] {1,2,2}, new int[] {2}))); // {1}
+        System.out.println(Arrays.toString(arrayDiff(new int[] {1,2,2}, new int[] {}))); // {1,2,2}
+        System.out.println(Arrays.toString(arrayDiff(new int[] {}, new int[] {1,2}))); // {}
+    }
+    public static int[] arrayDiff(int[] a, int[] b) {
+
+        int[] arr = new int[];
+
+        return arr;
+    }
+}
---
+++ src/SimpleMultiplication.java
@@ -0,0 +1,11 @@
+public class SimpleMultiplication {
+
+    public static int simpleMultiplication(int n) {
+
+        if (n % 2 == 0) {
+            return n *= 8;
+        } else {
+            return n *= 9;
+        }
+    }
+}
