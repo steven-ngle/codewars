fa11128aff61ce1462a596d057f551fbf5e29522
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-09-05 14:08:20 +0200

    commit

---
+++ src/isThisATriangle.java
@@ -0,0 +1,11 @@
+public class isThisATriangle {
+
+    public static boolean isTriangle(int a, int b, int c){
+
+        if (a + b > c && a + c > b && b + c > a) {
+            return true;
+        }
+
+        return false;
+    }
+}
