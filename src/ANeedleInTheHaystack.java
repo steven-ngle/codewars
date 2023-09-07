04e8ff19bf87f60b4a8ea7ff13bdcc342d479ea5
Steven Nguyen <stengu51202@gmail.com>
2023-09-07 20:08:20 +0200

    commit

---
+++ src/ANeedleInTheHaystack.java
@@ -0,0 +1,15 @@
+public class ANeedleInTheHaystack {
+
+    public static String findNeedle(Object[] haystack) {
+
+        for (int i = 0; i < haystack.length; i++) {
+            if (haystack[i] == null) {
+                continue;
+            }
+            if (haystack[i].equals("needle")) {
+                return "found the needle at position " + i;
+            }
+        }
+        return "Hi";
+    }
+}
