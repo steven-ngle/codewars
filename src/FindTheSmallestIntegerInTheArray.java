2412db87737e9211ec63e4239c6d6e6f981c06a1
Steven Nguyen <stengu51202@gmail.com>
2023-09-07 20:21:30 +0200

    commit

---
+++ src/FindTheSmallestIntegerInTheArray.java
@@ -0,0 +1,14 @@
+public class FindTheSmallestIntegerInTheArray {
+
+    public static int findSmallestInt(int[] args) {
+
+        int min = args[0];
+
+        for (int i = 1; i < args.length; i++) {
+            if (min > args[i]) {
+                min = args[i];
+            }
+        }
+        return min;
+    }
+}
