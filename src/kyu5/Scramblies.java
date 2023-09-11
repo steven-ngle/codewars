32f34d7fb53eb7a8e8501e5fc73b64a81ab92918
Steven Nguyen <stengu51202@gmail.com>
2023-09-11 19:38:52 +0200

    Scramblies

---
+++ src/kyu5/Scramblies.java
@@ -0,0 +1,27 @@
+package kyu5;
+
+import java.util.HashMap;
+
+public class Scramblies {
+
+    public static boolean scramble(String str1, String str2) {
+
+        int[] count = new int[26];
+
+
+        for (char c : str1.toCharArray()) {
+            int index = c - 'a';
+            count[index]++;
+        }
+
+        for (char c : str2.toCharArray()) {
+            int index = c - 'a';
+            if (count[index] == 0) {
+                return false;
+            }
+            count[index]--;
+        }
+
+        return true;
+    }
+}
