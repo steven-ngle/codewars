282c1e9534a1b60e714ded1aa95939e4235e72b6
Steven Nguyen <stengu51202@gmail.com>
2023-10-28 12:00:17 +0200

    FakeBinary

---
+++ src/kyu8/FakeBinary.java
@@ -0,0 +1,25 @@
+package kyu8;
+
+public class FakeBinary {
+
+    public static void main(String[] args) {
+        System.out.println(fakeBin("3149"));
+    }
+
+    public static String fakeBin(String numberString) {
+
+        char[] ch = numberString.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] < '5') {
+                ch[i] = '0';
+            } else {
+                ch[i] = '1';
+            }
+        }
+
+        String res = new String(ch);
+
+        return res;
+    }
+}
