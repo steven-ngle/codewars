2c8132c2a7e5ed53acaaf291a4da7b059bc6d51c
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-11-05 17:28:40 +0100

    5.11.23

---
+++ src/kyu8/GrasshopperSummation.java
@@ -0,0 +1,14 @@
+package kyu8;
+
+public class GrasshopperSummation {
+
+    public static int summation(int n) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
